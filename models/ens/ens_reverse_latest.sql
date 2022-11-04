cc778d3fc0ab07d8724a9ec5dc176bf007555872
Allen Day <allenday@allenday.com>
2022-11-04 09:53:43 +0800

    quote reserved word

--- models/ens/ens_reverse_latest.sql
+++ models/ens/ens_reverse_latest.sql
@@ -37,8 +37,8 @@ with node_names as (
     output as node
     from {{ source('ethereum', 'traces') }} tr
     where success
-        and (to = '0x9062c0a6dbd6108336bcbe4593a3d1ce05512069' -- ReverseRegistrar v1
-            or  to = '0x084b1c3c81545d370f3634392de611caabff8148' -- ReverseRegistrar v2
+        and (`to` = '0x9062c0a6dbd6108336bcbe4593a3d1ce05512069' -- ReverseRegistrar v1
+            or  `to` = '0x084b1c3c81545d370f3634392de611caabff8148' -- ReverseRegistrar v2
         )
         and substring(input,1,10) in (
             '0xc47f0027' -- setName(string)
